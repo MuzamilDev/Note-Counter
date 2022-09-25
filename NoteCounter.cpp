636d119ea7c74699804349959d1ecfa3eb43e635
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-09-25 16:21:38 +0500

    Add files via upload

---
+++ NoteCounter.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, money;
+
+    cout<<"Enter Number of Notes "<<endl;
+    cin>>n;
+
+    cout<<"Enter Money (i,e 100,500) "<<endl;
+    cin>>money;
+
+    cout<<n*money<<endl;
+
+    return 0;
+}
